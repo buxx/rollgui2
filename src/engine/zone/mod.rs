6c49042fac60462e410c432acc0708f6b231ecc7
Bastien Sevajol <bastien.sevajol@algoo.fr>
2022-01-31 17:53:38 +0100

    impelment zoom by keyboard

--- src/engine/zone/mod.rs
+++ src/engine/zone/mod.rs
@@ -16,6 +16,8 @@ pub struct ZoneEngine {
     pub state: state::ZoneState,
     pub tick_last: f64,
     pub tick_i: i16,
+    pub zoom_mode: ZoomMode,
+    pub last_limited_user_input: f64,
 }
 
 impl ZoneEngine {
@@ -25,6 +27,8 @@ impl ZoneEngine {
             state,
             tick_last: get_time(),
             tick_i: 0,
+            zoom_mode: ZoomMode::Normal,
+            last_limited_user_input: get_time(),
         }
     }
 
@@ -50,6 +54,20 @@ impl ZoneEngine {
             match user_input {
                 UserInput::MovePlayerBy(vector) => {
                     player_acceleration += vector;
+                }
+                UserInput::ZoomIn => {
+                    self.zoom_mode = match self.zoom_mode {
+                        ZoomMode::Normal => ZoomMode::Double,
+                        ZoomMode::Double => ZoomMode::Double,
+                        ZoomMode::Large => ZoomMode::Normal,
+                    }
+                }
+                UserInput::ZoomOut => {
+                    self.zoom_mode = match self.zoom_mode {
+                        ZoomMode::Normal => ZoomMode::Large,
+                        ZoomMode::Double => ZoomMode::Normal,
+                        ZoomMode::Large => ZoomMode::Large,
+                    }
                 }
             }
         }
@@ -81,10 +99,10 @@ impl ZoneEngine {
         self.state.player_display.running = player_running;
     }
 
-    fn user_inputs(&self) -> Vec<UserInput> {
+    fn user_inputs(&mut self) -> Vec<UserInput> {
         let mut user_inputs = Vec::new();
 
-        // Keyboard inputs
+        // Keyboard inputs without repetition limitation
         if is_key_down(KeyCode::Up) {
             user_inputs.push(UserInput::MovePlayerBy(Vec2::new(0., -1.)));
         }
@@ -98,6 +116,18 @@ impl ZoneEngine {
             user_inputs.push(UserInput::MovePlayerBy(Vec2::new(1., 0.)));
         }
 
+        // Keyboard inputs with repetition limitation
+        if get_time() - self.last_limited_user_input > 0.5 {
+            if is_key_down(KeyCode::I) {
+                user_inputs.push(UserInput::ZoomIn);
+                self.last_limited_user_input = get_time();
+            }
+            if is_key_down(KeyCode::O) {
+                user_inputs.push(UserInput::ZoomOut);
+                self.last_limited_user_input = get_time();
+            }
+        }
+
         // Mouse inputs
         if is_mouse_button_down(MouseButton::Left) {
             user_inputs.push(UserInput::MovePlayerBy(mouse_position_local() * 2.0));
@@ -107,8 +137,14 @@ impl ZoneEngine {
     }
 
     fn camera(&self) {
-        let zoom_x = (self.state.map.concrete_width / screen_width()) * 2.;
-        let zoom_y = (self.state.map.concrete_height / screen_height()) * 2.;
+        let zoom_multiplier = match self.zoom_mode {
+            ZoomMode::Large => 1.,
+            ZoomMode::Normal => 2.,
+            ZoomMode::Double => 4.,
+        };
+
+        let zoom_x = (self.state.map.concrete_width / screen_width()) * zoom_multiplier;
+        let zoom_y = (self.state.map.concrete_height / screen_height()) * zoom_multiplier;
 
         let target_x = self.state.player_display.position.x / self.state.map.concrete_width;
         // Invert Y axis because the camera is Y inverted
@@ -161,4 +197,12 @@ pub enum PlayerRunning {
 
 pub enum UserInput {
     MovePlayerBy(Vec2),
+    ZoomIn,
+    ZoomOut,
+}
+
+pub enum ZoomMode {
+    Normal,
+    Double,
+    Large,
 }
