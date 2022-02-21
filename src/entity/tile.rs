e74cd6f363d6d3a95598399b0ddebed934f94bd4
Bastien Sevajol <bastien.sevajol@algoo.fr>
2022-02-21 23:42:56 +0100

    modify player move by hump: missing file

---
+++ src/entity/tile.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TransportMode {
+    #[serde(rename = "WALKING")]
+    Walking,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum HumpType {
+    #[serde(rename = "NORMAL")]
+    Normal,
+    #[serde(rename = "SLOW")]
+    Slow,
+    #[serde(rename = "VERY_SLOW")]
+    VerySlow,
+}
+
+impl HumpType {
+    pub fn to_speed(&self) -> f32 {
+        match self {
+            HumpType::Normal => 1.0,
+            HumpType::Slow => 0.75,
+            HumpType::VerySlow => 0.5,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Tile {
+    pub id: String,
+    pub char: char,
+    pub traversable: HashMap<TransportMode, bool>,
+    pub hump: HashMap<TransportMode, HumpType>,
+}
